585ffc5cfa442db0727d6f8d609511bc9a0a1c9f
alijumanov <jumanovsafarali@gmail.com>
2023-06-06 10:22:15 +0500

    API urls fixed

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,10 +1,94 @@
 import axios from "axios";
-import { NEWS_URL, PARTNERS_URL } from "./ApiUrl";
+import { ATHLETES_URL, BRANCHES_URL, COACHES_URL, DEPARTMENTS_URL, DOCUMENTS_URL, NEWS_URL, ORGANIZATIONS_URL, PARTNERS_URL, POOLS_URL, RECORDS_URL, REFEREES_URL, REGIONS_URL, RULES_URL, STAFF_URL, TEAMS_URL, TOURNAMENTS_URL } from "./ApiUrl";
 
 // API of News
 
 export const fetchNews = () => {
     return axios.get(NEWS_URL)
+};
+
+// API of Coaches
+
+export const fetchCoaches = () => {
+    return axios.get(COACHES_URL)
+};
+
+// API of Referees
+
+export const fetchReferees = () => {
+    return axios.get(REFEREES_URL)
+};
+
+// API of Athletes
+
+export const fetchAthletes = () => {
+    return axios.get(ATHLETES_URL)
+};
+
+// API of Records
+
+export const fetchRecords = () => {
+    return axios.get(RECORDS_URL)
+};
+
+// API of Tournaments
+
+export const fetchTournaments = () => {
+    return axios.get(TOURNAMENTS_URL)
+};
+
+// API of Teams
+
+export const fetchTeams = () => {
+    return axios.get(TEAMS_URL)
+};
+
+// API of Branches
+
+export const fetchBranches = () => {
+    return axios.get(BRANCHES_URL)
+};
+
+// API of Departments
+
+export const fetchDepartments = () => {
+    return axios.get(DEPARTMENTS_URL)
+};
+
+// API of Documents
+
+export const fetchDocuments = () => {
+    return axios.get(DOCUMENTS_URL)
+};
+
+// API of Organizations
+
+export const fetchOrganizations = () => {
+    return axios.get(ORGANIZATIONS_URL)
+};
+
+// API of Pools
+
+export const fetchPools = () => {
+    return axios.get(POOLS_URL)
+};
+
+// API of Rules
+
+export const fetchRules = () => {
+    return axios.get(RULES_URL)
+};
+
+// API of Regions
+
+export const fetchRegions = () => {
+    return axios.get(REGIONS_URL)
+};
+
+// API of Staff
+
+export const fetchStaff = () => {
+    return axios.get(STAFF_URL)
 };
 
 // API of Partners
--- src/api/ApiUrl.js
+++ src/api/ApiUrl.js
@@ -1,4 +1,18 @@
 const BASE_URL = `https://usfed.herokuapp.com/api`;
 
-export const NEWS_URL = `${BASE_URL}/news/`;
-export const PARTNERS_URL = `${BASE_URL}/partners/`;
+export const NEWS_URL = `${BASE_URL}/news/`;        //Новости
+export const TEAMS_URL = `${BASE_URL}/teams/`;      //Сборные команды
+export const POOLS_URL = `${BASE_URL}/pools/`;      //Бассейны
+export const RULES_URL = `${BASE_URL}/rules/`;      //Правила
+export const STAFF_URL = `${BASE_URL}/staff/`;      //Сотрудники федерации
+export const COACHES_URL = `${BASE_URL}/coaches/`;      //тренеры
+export const RECORDS_URL = `${BASE_URL}/records/`;      //Рекорды
+export const REGIONS_URL = `${BASE_URL}/regions/`;      //Правила
+export const REFEREES_URL = `${BASE_URL}/referees/`;    //судьи
+export const ATHLETES_URL = `${BASE_URL}/athletes/`;    //спортсмены
+export const BRANCHES_URL = `${BASE_URL}/branches/`;    //Филиалы федерации
+export const PARTNERS_URL = `${BASE_URL}/partners/`;    //Сотрудники федерации
+export const DOCUMENTS_URL = `${BASE_URL}/documents/`;          //Документы
+export const TOURNAMENTS_URL = `${BASE_URL}/tournaments/`;      //Соревнования
+export const DEPARTMENTS_URL = `${BASE_URL}/departments/`;      //Отделы федерации
+export const ORGANIZATIONS_URL = `${BASE_URL}/organizations/`;  //Организации (выдают сертификаты/документы)
